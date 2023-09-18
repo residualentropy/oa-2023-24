9c91a2e6bdbab0e549bc98101a6acb5ffc79dfda
traitboundnotsatisfied <144365472+traitboundnotsatisfied@users.noreply.github.com>
2023-09-17 21:56:31 -0500

    its perfect...

--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -1,9 +1,89 @@
 const graphDiv = document.getElementById("graph");
 
-const urlparams = new URLSearchParams(window.location.search);
-const backend_urls = { "local": "http://localhost:3000" };
-const BACKEND_URL = backend_urls[urlparams.get("backendtype")];
+const BACKEND_URL = "https://oa-backend.residualentropy.repl.co";
 
-fetch(BACKEND_URL).then(async res => {
-    Plotly.newPlot( graphDiv, [ await res.json() ]); 
-})
+const FOOD_SAFETY_TEMP_C = 4;
+
+function setHeader(s) {
+    document.getElementById("header").innerHTML = s;
+}
+
+async function drawPlot() {
+    let temps = fetch(`${BACKEND_URL}/api/r/temps_recent`);
+    temps = await (await temps).json();
+    let names = fetch(`${BACKEND_URL}/api/r/sensor_names`);
+    names = await (await names).json();
+    let traces = {
+        "_foodsafety": {
+            name: "Maximum Food-safe Temperature",
+            x: [],
+            y: [],
+            type: 'scatter',
+            line: {
+                color: 'red',
+                width: 5,
+            },
+        },
+    };
+    let ids = [];
+    let total_sum = 0;
+    let total_count = 0;
+    for (const [id, name] of Object.entries(names.names)) {
+        traces[id] = {
+            name,
+            x: [],
+            y: [],
+            type: 'scatter',
+        };
+        if (name === "Evaporator") {
+            traces[id].line = {
+                color: 'gray',
+                dash: 'dot',
+            };
+        }
+        ids.push(id);
+    }
+    for (const temps_instance of temps.recent) {
+        let x = new Date(temps_instance.unixts * 1000);
+        for (const id of ids) {
+            let y = temps_instance.readings[id];
+            traces[id].x.push(x);
+            traces[id].y.push(y);
+            if (traces[id].name !== "Evaporator") {
+                total_sum += y;
+                total_count += 1;
+            }
+        }
+        traces["_foodsafety"].x.push(x);
+        traces["_foodsafety"].y.push(FOOD_SAFETY_TEMP_C);
+    }
+    let total_avg = total_sum / total_count;
+    console.log("Total Average Temperature (inside fridge, degs C):", total_avg);
+    let traces_without_evaporator = Object.values(traces);
+    let evaporator_trace;
+    for (let i = 0; i < traces_without_evaporator.length; i++) {
+        if (traces_without_evaporator[i].name === "Evaporator") {
+            evaporator_trace = traces_without_evaporator.splice(i, 1)[0];
+            break;
+        }
+    }
+    console.log(traces_without_evaporator);
+    console.log(evaporator_trace);
+    Plotly.newPlot('graph1', traces_without_evaporator, {
+        title: 'Temperatures Inside Minifridge',
+        width: window.innerWidth,
+        height: window.innerHeight * (1/2),
+    });
+    Plotly.newPlot('graph2', [traces["_foodsafety"], evaporator_trace], {
+        title: 'Temperature of Evaporator',
+        width: window.innerWidth,
+        height: window.innerHeight * (1/3),
+    });
+    if (total_avg <= FOOD_SAFETY_TEMP_C) {
+        setHeader("Is my fridge working? <i><span style=\"color:green;\">YES!</i>");
+    } else {
+        setHeader("Is my fridge working? <i><span style=\"color:red;\">NO!</span><i>");
+    }
+}
+
+setInterval(drawPlot, 2000);
